9381090278608f759801fb94af0259eff8c314e7
rio-hashicorp <rio.sibuea@hashicorp.com>
2024-05-15 08:17:18 +0700

    update

--- main.tf
+++ main.tf
@@ -110,5 +110,5 @@ output "Vault_GCP_Private_IP_Address" {
 }
 
 output "Vault_GCP_Public_IP_Address" {
-  value = google_compute_instance.vault_gcp.access_config.nat_ip
+  value = google_compute_instance.vault_gcp.access_config.0.nat_ip
 }
